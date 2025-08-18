f10ad172053c0ff1694d3220a0b2c1afa76ee8a4
td4420 <trongdao2000@gmail.com>
2025-08-18 18:10:22 +0700

    Move notify function to lib

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -1,3 +1,23 @@
 def call(String message) {
     echo "📢 Sending notification: ${message}"
 }
+
+def notifyCreatePrGoLive(remotes) {
+    // pull all stashed files into THIS workspace
+    for (int i = 0; i < remotes.size(); i++) {
+        unstash "prs-${remotes[i]}"
+    }
+
+    def msgLines = []
+    // Use index-based loop to avoid capturing a non-serializable iterator
+    for (int i = 0; i < remotes.size(); i++) {
+        def remote = remotes[i]
+        def file = "prs-${remote}.txt"
+        msgLines << "**PRs created for ${remote} PWA**".toUpperCase()
+        if (fileExists(file)) {
+            msgLines << "- ${readFile(file).trim()}"
+        }
+    }
+
+    notify(msgLines.join("\n"))
+}
