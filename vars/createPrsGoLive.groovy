e79e4e13417ed783867fb75da0e74c499868b14a
td4420 <trongdao2000@gmail.com>
2025-08-19 09:38:14 +0700

    Update structure

---
+++ vars/createPrsGoLive.groovy
@@ -0,0 +1,70 @@
+def call() {
+    def repoUrls = (env.PWA_REPO_URLS ?: "")
+        .split("###")
+        .findAll { it }
+
+    def remotes = (params.REMOTES ?: '')
+        .split(',')
+        .collect { it.trim() }
+        .findAll { it }
+
+    def pwaProcessors = generateProcessor(repoUrls, remotes)
+    def m2Processors = generateProcessor([env.M2_REPO_URL], ['M2'])
+
+    //Run the PWA processors in parallel
+    parallel pwaProcessors
+    notify.notifyCreatePrGoLive(remotes)
+
+    //Run the M2 processor in parallel
+    parallel m2Processors
+    notify.notifyCreatePrGoLive(['M2'])
+}
+
+def createPullRequestGoLiveFullFlow(repoUrl) {
+    def branches = params.FEATURE_BRANCH
+        .split(',')
+        .collect { it.trim() }
+        .findAll { it }
+
+    def featurePrs = []
+    def featurePr = ''
+    def goLivePr = ''
+
+    //Create release branch if not existed
+    git.createBranch(env.GITHUB_CREDENTIALS_ID, repoUrl, params.RELEASE_BRANCH, env.MAIN_BRANCH_PWA)
+
+    branches.each { featureBranch ->
+        // Create pull request from feature branch into release branch and auto merge
+        featurePr = git.createPullRequestFlow(env.GITHUB_CREDENTIALS_ID, repoUrl, featureBranch, params.RELEASE_BRANCH, true)
+        featurePrs << "- PR merge ${featureBranch} into ${params.RELEASE_BRANCH} : ${featurePr}"
+    }
+
+    //Create pull request from release branch into main/master branch
+    goLivePr = git.createPullRequestFlow(env.GITHUB_CREDENTIALS_ID, repoUrl, params.RELEASE_BRANCH, env.MAIN_BRANCH_PWA, false)
+
+    def result = []
+    if (goLivePr) {
+        result << "- PR merge ${params.RELEASE_BRANCH} into ${env.MAIN_BRANCH_PWA} : ${goLivePr}"
+    }
+
+    result.addAll(featurePrs)
+
+    return result.join("\n")
+}
+
+def generateProcessor(repoUrls, remotes) {
+    def branches = [:]
+    repoUrls.eachWithIndex { repoUrl, index ->
+        branches["Create-PR-${remotes[index]}"] = {
+            node {
+                def prLines = createPullRequestGoLiveFullFlow(repoUrl)
+                // persist this branch’s result so we can aggregate after parallel
+                def outFile = "prs-${remotes[index]}.txt"
+                writeFile file: outFile, text: (prLines instanceof List ? prLines.join("\n") : "${prLines}")
+                stash name: "prs-${remotes[index]}", includes: outFile, useDefaultExcludes: false
+            }
+        }
+    }
+
+    return branches
+}
